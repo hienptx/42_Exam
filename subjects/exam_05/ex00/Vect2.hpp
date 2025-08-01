e97617b83dd1eb9712edaeae940c7c603105c8c5
Hien Pham <hipham@1-B-12.42heilbronn.de>
2025-08-01 18:13:50 +0200

    update ex00

--- subjects/exam_05/ex00/Vect2.hpp
+++ subjects/exam_05/ex00/Vect2.hpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <iostream>
 
 class vect2 {
@@ -6,23 +5,34 @@ private:
     int data[2];
 
 public:
-    vect2(int x = 0, int y = 0) {
-		data[0]= x;
-		data[1] =y;
-	}
-    
-    vect2 operator+(const vect2& other) const {
-        return vect2(data[0] + other.data[0], data[1] + other.data[1]);
+    // Default constructor
+    vect2() {
+        data[0] = 0;
+        data[1] = 0;
     }
     
-    vect2 operator-(const vect2& other) const {
-        return vect2(data[0] - other.data[0], data[1] - other.data[1]);
+    // Parameterized constructor
+    vect2(int x, int y) {
+        data[0] = x;
+        data[1] = y;
     }
     
-    vect2 operator*(int scalar) const {
-        return vect2(data[0] * scalar, data[1] * scalar);
+    // Copy constructor
+    vect2(const vect2& other) {
+        data[0] = other.data[0];
+        data[1] = other.data[1];
     }
     
+    // Assignment operator
+    vect2& operator=(const vect2& other) {
+        if (this != &other) {
+            data[0] = other.data[0];
+            data[1] = other.data[1];
+        }
+        return *this;
+    }
+    
+    // Index operator for accessing components
     int& operator[](int index) {
         return data[index];
     }
@@ -31,8 +41,92 @@ public:
         return data[index];
     }
     
+    // Addition operator
+    vect2 operator+(const vect2& other) const {
+        return vect2(data[0] + other.data[0], data[1] + other.data[1]);
+    }
+    
+    // Subtraction operator
+    vect2 operator-(const vect2& other) const {
+        return vect2(data[0] - other.data[0], data[1] - other.data[1]);
+    }
+    
+    // Scalar multiplication operator
+    vect2 operator*(int scalar) const {
+        return vect2(data[0] * scalar, data[1] * scalar);
+    }
+    
+    // Unary minus operator
+    vect2 operator-() const {
+        return vect2(-data[0], -data[1]);
+    }
+    
+    // Compound assignment operators
+    vect2& operator+=(const vect2& other) {
+        data[0] += other.data[0];
+        data[1] += other.data[1];
+        return *this;
+    }
+    
+    vect2& operator-=(const vect2& other) {
+        data[0] -= other.data[0];
+        data[1] -= other.data[1];
+        return *this;
+    }
+    
+    vect2& operator*=(int scalar) {
+        data[0] *= scalar;
+        data[1] *= scalar;
+        return *this;
+    }
+    
+    // Increment operators
+    vect2& operator++() { // prefix
+        ++data[0];
+        ++data[1];
+        return *this;
+    }
+    
+    vect2 operator++(int) { // postfix
+        vect2 temp(*this);
+        ++data[0];
+        ++data[1];
+        return temp;
+    }
+    
+    // Decrement operators
+    vect2& operator--() { // prefix
+        --data[0];
+        --data[1];
+        return *this;
+    }
+    
+    vect2 operator--(int) { // postfix
+        vect2 temp(*this);
+        --data[0];
+        --data[1];
+        return temp;
+    }
+    
+    // Equality operator
+    bool operator==(const vect2& other) const {
+        return (data[0] == other.data[0]) && (data[1] == other.data[1]);
+    }
+    
+    // Inequality operator
+    bool operator!=(const vect2& other) const {
+        return !(*this == other);
+    }
 };
 
+// Output stream operator
 std::ostream& operator<<(std::ostream& os, const vect2& v) {
-    return os << "{" << v[0] << ", " << v[1] << "}";
+    os << "{" << v[0] << ", " << v[1] << "}";
+    return os;
 }
+
+// Scalar multiplication (scalar * vector)
+vect2 operator*(int scalar, const vect2& v) {
+    return vect2(v[0] * scalar, v[1] * scalar);
+}
+
